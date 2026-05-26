a516325d9336f7298b89c190c591ac4235718fe0
methodelogistiquesncf-boop <methodelogistiquesncf@gmail.com>
2026-05-26 10:24:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // COMPLET CONFORME — Service Worker
 // ═══════════════════════════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'completconforme-v2.4'; // ← bump pour invalider l'ancien cache
+const CACHE_NAME = 'completconforme-v2.5'; // ← bump pour invalider l'ancien cache
 
 const PRECACHE_ASSETS = [
     './',
